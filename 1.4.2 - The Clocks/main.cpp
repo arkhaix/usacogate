e388e9eff84a2f3beb1ff80e0bc34db311209d55
Andrew Gray <arkhaix@users.noreply.github.com>
2013-02-06 22:23:35 -0800

    [clocks] - Solved "The Clocks". I gave up on the fancy bfs and dfs+id approaches and just brute forced it instead. There are only 4^9 states.

--- 1.4.2 - The Clocks/main.cpp
+++ 1.4.2 - The Clocks/main.cpp
@@ -29,19 +29,6 @@ struct State
 		Depth = 0;
 	}
 
-	long long ToKey()
-	{
-		long long key = 0;
-
-		for(int i=0;i<9;i++)
-		{
-			key *= 12;
-			key += Clocks[i];
-		}
-
-		return key;
-	}
-
 	bool isSolved()
 	{
 		for(int i=0;i<9;i++)
@@ -51,94 +38,94 @@ struct State
 		return true;
 	}
 
-	State Move1()
+	void Move1()
 	{
 		State res = *this;
 		res.Clocks[0] += 3; res.Clocks[1] += 3; res.Clocks[3] += 3; res.Clocks[4] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 1;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 
-	State Move2()
+	void Move2()
 	{
 		State res = *this;
 		res.Clocks[0] += 3; res.Clocks[1] += 3; res.Clocks[2] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 2;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 
-	State Move3()
+	void Move3()
 	{
 		State res = *this;
 		res.Clocks[1] += 3; res.Clocks[2] += 3; res.Clocks[4] += 3; res.Clocks[5] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 3;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 
-	State Move4()
+	void Move4()
 	{
 		State res = *this;
 		res.Clocks[0] += 3; res.Clocks[3] += 3; res.Clocks[6] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 4;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 
-	State Move5()
+	void Move5()
 	{
 		State res = *this;
 		res.Clocks[1] += 3; res.Clocks[3] += 3; res.Clocks[4] += 3; res.Clocks[5] += 3; res.Clocks[7] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 5;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 
-	State Move6()
+	void Move6()
 	{
 		State res = *this;
 		res.Clocks[2] += 3; res.Clocks[5] += 3; res.Clocks[8] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 6;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 
-	State Move7()
+	void Move7()
 	{
 		State res = *this;
 		res.Clocks[3] += 3; res.Clocks[4] += 3; res.Clocks[6] += 3; res.Clocks[7] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 7;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 
-	State Move8()
+	void Move8()
 	{
 		State res = *this;
 		res.Clocks[6] += 3; res.Clocks[7] += 3; res.Clocks[8] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 8;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 
-	State Move9()
+	void Move9()
 	{
 		State res = *this;
 		res.Clocks[4] += 3; res.Clocks[5] += 3; res.Clocks[7] += 3; res.Clocks[8] += 3;
 		for(int i=0;i<9;i++) res.Clocks[i] %= 12;
 		res.Path[ res.Depth ] = 9;
 		res.Depth++;
-		return res;
+		*this = res;
 	}
 };
 
@@ -157,63 +144,23 @@ bool StateByPath(const State& a, const State& b)
 }
 
 vector<State> solutions;
-set<int> visited;
-void Dfs(int maxDepth, State& curState)
-{
-	if(curState.Depth > maxDepth)
-		return;
-
-	if(curState.isSolved())
-	{
-		solutions.push_back(curState);
-		return;
-	}
-
-	int key = curState.ToKey();
-	set<int>::iterator keyIter = visited.find(key);
-
-	if(keyIter != visited.end())
-		return;
-
-	visited.insert(key);
-
-	State nextState = curState.Move1();
-	Dfs(maxDepth, nextState);
-
-	nextState = curState.Move2();
-	Dfs(maxDepth, nextState);
-
-	nextState = curState.Move3();
-	Dfs(maxDepth, nextState);
-
-	nextState = curState.Move4();
-	Dfs(maxDepth, nextState);
-
-	nextState = curState.Move5();
-	Dfs(maxDepth, nextState);
-
-	nextState = curState.Move6();
-	Dfs(maxDepth, nextState);
-
-	nextState = curState.Move7();
-	Dfs(maxDepth, nextState);
-
-	nextState = curState.Move8();
-	Dfs(maxDepth, nextState);
-
-	nextState = curState.Move9();
-	Dfs(maxDepth, nextState);
-}
-
 string Go(State startState)
 {
-	int maxDepth = 0;
-	while(solutions.empty())
-	{
-		visited.clear();
-		maxDepth++;
-		Dfs(maxDepth, startState);
-	}
+	int x;
+	State state[9];
+
+	for(int a=0;a<4;a++) { state[0] = startState; for(x=0;x<a;x++) state[0].Move1(); 
+	for(int b=0;b<4;b++) { state[1] = state[0]; for(x=0;x<b;x++) state[1].Move2();
+	for(int c=0;c<4;c++) { state[2] = state[1]; for(x=0;x<c;x++) state[2].Move3();
+	for(int d=0;d<4;d++) { state[3] = state[2]; for(x=0;x<d;x++) state[3].Move4();
+	for(int e=0;e<4;e++) { state[4] = state[3]; for(x=0;x<e;x++) state[4].Move5();
+	for(int f=0;f<4;f++) { state[5] = state[4]; for(x=0;x<f;x++) state[5].Move6();
+	for(int g=0;g<4;g++) { state[6] = state[5]; for(x=0;x<g;x++) state[6].Move7();
+	for(int h=0;h<4;h++) { state[7] = state[6]; for(x=0;x<h;x++) state[7].Move8();
+	for(int i=0;i<4;i++) { state[8] = state[7]; for(x=0;x<i;x++) state[8].Move9();
+		if(state[8].isSolved())
+			solutions.push_back(state[8]);
+	} } } } } } } } }
 
 	sort(solutions.begin(), solutions.end(), StateByPath);
 	State bestSolution = solutions.front();
