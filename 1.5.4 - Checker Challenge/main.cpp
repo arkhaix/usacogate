19aa7cea4686aabdae4fb6ec2c34eb19b782a9ec
Andrew Gray <arkhaix@users.noreply.github.com>
2013-02-10 21:51:08 -0800

    [checker] - Saving progress on "Checker Challenge".  This solution is too slow on n=13.

---
+++ 1.5.4 - Checker Challenge/main.cpp
@@ -0,0 +1,109 @@
+/*
+ID: general10
+PROG: checker
+LANG: C++
+*/
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+vector<string> solutions;
+int numSolutions;
+int board[13][13] = {0};
+int n;
+
+string Save()
+{
+	string res;
+	char buf[8];
+
+	for(int r=0;r<n;r++) for(int c=0;c<n;c++) 
+	{
+		if(board[r][c])
+		{
+			sprintf(buf, "%d", c+1);
+			res += buf;
+			if(r != n-1)
+				res += " ";
+			break;
+		}
+	}
+
+	return res;
+}
+
+void Go(int depth)
+{
+	int r = depth;
+	if(r == n)
+	{
+		if(solutions.size() < 3)
+			solutions.push_back(Save());
+		numSolutions++;
+		return;
+	}
+
+	for(int c=0;c<n;c++)
+	{
+		bool valid = true;
+		for(int i=0;i<n && valid;i++)
+			if(board[i][c]) 
+				valid = false; 
+
+		for(int i=0;(r+i)<n && (c+i)<n && valid;i++)
+			if(board[r+i][c+i])
+				valid = false;
+
+		for(int i=0;(r-i)>=0 && (c-i)>=0 && valid;i++)
+			if(board[r-i][c-i])
+				valid = false;
+
+		for(int i=0;(r+i)<n && (c-i)>=0 && valid;i++)
+			if(board[r+i][c-i])
+				valid = false;
+
+		for(int i=0;(r-i)>=0 && (c+i)<n && valid;i++)
+			if(board[r-i][c+i])
+				valid = false;
+
+		if(valid)
+		{
+			board[r][c]=1;
+			Go(depth+1);
+			board[r][c]=0;
+		}
+	}
+}
+
+int main() 
+{
+	FILE *fin  = fopen("checker.in", "r");
+	FILE *fout = fopen("checker.out", "w");
+
+	fscanf(fin, "%d\n", &n);
+
+	Go(0);
+
+	for(int i=0;i<solutions.size();i++)
+	{
+		fprintf(fout, "%s\n", solutions[i].c_str());
+	}
+
+	fprintf(fout, "%d\n", numSolutions);
+
+	fclose(fout);
+	fclose(fin);
+
+	return 0;
+}
