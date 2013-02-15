fa9e3b264f2fc535dc1cb7ed21dcee08fcc0ebce
Andrew Gray <arkhaix@users.noreply.github.com>
2013-02-15 09:14:49 -0800

    [sort3] - Solved "Sorting a Three-Valued Sequence".  This method is very inefficient since it sorts the whole sequence first, but it's much more readable and easier to understand than my earlier approach.

--- 2.1.3 - Sorting a Three-Valued Sequence/main.cpp
+++ 2.1.3 - Sorting a Three-Valued Sequence/main.cpp
@@ -26,50 +26,50 @@ int main()
 	int n;
 	fscanf(fin, "%d\n", &n);
 
-	int count[3] = {0};
-	int sequence[1000];
+	vector<int> sequence;
+	sequence.reserve(1000);
 	for(int i=0;i<n;i++)
 	{
-		fscanf(fin, "%d\n", &sequence[i]);
-		count[sequence[i]-1]++;
+		int num;
+		fscanf(fin, "%d\n", &num);
+		sequence.push_back(num);
 	}
 
-	int startPos[3] = {0, count[0], count[0]+count[1]};
-	int pos[3] = {startPos[0], startPos[1], startPos[1]};
-	int endPos[3] = {startPos[1], startPos[2], n};
+	vector<int> sortedSequence = sequence;
+	sort(sortedSequence.begin(), sortedSequence.end());
 
 	int res = 0;
-	for(;pos[0]<endPos[0];pos[0]++)
+	int bad = 0;
+
+	for(int i=0;i<sequence.size();i++)
 	{
-		if(sequence[ pos[0] ] == 2)
+		if(sequence[i] == sortedSequence[i])
+			continue;
+
+		bool foundSwap = false;
+
+		for(int j=0;j<n;j++)
 		{
-			for(; sequence[ pos[1] ] != 1; pos[1]++) {}
-			swap(sequence[ pos[0] ], sequence[ pos[1] ]);
-			res++;
-		}
-		else if(sequence[ pos[0] ] == 3)
-		{
-			for(; sequence[ pos[2] ] != 1 && pos[2] < endPos[2]; pos[2]++) {}
-			if(pos[2] >= endPos[2])
+			if(sequence[i] == sortedSequence[j] && sequence[j] == sortedSequence[i])
 			{
-				pos[2] = startPos[1];
-				for(; sequence[ pos[2] ] != 1; pos[2]++) {}
+				swap(sequence[i], sequence[j]);
+				res++;
+				foundSwap = true;
+				break;
 			}
-			swap(sequence[ pos[0] ], sequence[ pos[2] ]);
-			res++;
 		}
-	}
 
-	pos[1] = startPos[1];
-	pos[2] = startPos[2];
-
-	for(;pos[1]<endPos[1];pos[1]++)
-	{
-		if(sequence[ pos[1] ] == 3)
+		if(foundSwap == false)
 		{
-			for(; sequence[ pos[2] ] != 2; pos[2]++) {}
-			swap(sequence[ pos[1] ], sequence[ pos[2] ]);
-			res++;
+			for(int k=0;k<n;k++)
+			{
+				if(sequence[k] == sortedSequence[i] && sequence[k] != sortedSequence[k])
+				{
+					swap(sequence[i], sequence[k]);
+					res++;
+					break;
+				}
+			}
 		}
 	}
 
